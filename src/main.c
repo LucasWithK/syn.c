c3443b26ced6c0d7d655c2ee499b8ebb19e45749
LucasWithK <176577210+LucasWithK@users.noreply.github.com>
2025-06-05 16:09:29 +0200

    added str functionality

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define STR_IMPLEMENTATION
 #include "str.h"
 
 typedef struct {
@@ -10,15 +11,31 @@ typedef struct {
 
 int main(void) {
 
-    str s = {0};
+    strb sb = {0};
 
-    str_append(&s, "Hello, World");
+    strb_append(&sb, "Hello, World");
 
-    str_push(&s, '!');
+    strb_push(&sb, '!');
 
-    printf("CONTENT "STRV_FMT" END\n", STRV_ARG(s));
+    str s = strb_build(sb);
 
-    str_free(s);
+    printf("CONTENT "STR_FMT" END\n", STR_ARG(s));
+
+    str x = str_substr(s, 5, s.count);
+
+    printf("CONTENT "STR_FMT" END\n", STR_ARG(x));
+
+    putchar('0' + str_starts_with(s, x));
+    putchar('0' + str_starts_with(s, str_cstr("Hello")));
+    putchar('0' + str_starts_with(s, str_cstr("World")));
+    putchar('0' + str_starts_with(s, str_cstr("")));
+    putchar('0' + str_starts_with(s, s));
+    putchar('0' + str_starts_with(x, s));
+    putchar('0' + str_starts_with(x, str_cstr(",")));
+
+    putchar('\n');
+
+    strb_free(sb);
 
     return 0;
 }
--- src/str.h
+++ src/str.h
@@ -1,73 +1,117 @@
 /**
- * This file provides the macro implementations for a string and string view data structure.
- * A string is simply a dynamic array from `da.h` with the following shape:
+ * This file provides the implementations for string builder and view data structures.
+ *
+ * A string builder is simply a dynamic array from `da.h` with the following shape:
  *
  * ```c
  * typedef struct {
  *  char *items;
  *  size_t count;
  *  size_t capacity;
- * } str;
+ * } strb;
  * ```
  *
- * This means any `da_*` macros can be used on a string.
+ * This means any `da_*` macros can be used on a string builder.
  *
- * The following macros can be used on a string:
- *
- * `str_push(&str, char);`
- * `str_append(&str, cstr);`
- *
- * `str_free(str);`
- *
- * A string view is like a window into a string.
- * It is valid as long as the original string is not realoccated.
+ * A string view is like a window into any other string.
  * It has the following shape:
  *
  * ```c
  * typdef struct {
  *  char *items;
  *  size_t count;
- * } strv;
+ * } str;
  * ```
  *
- * The following macros can be used on a string and a string view:
+ * The following definitions can be used on a string builder:
  *
- * `STRV_ARG(strv)` used togheter with `STRV_FMT` for `printf` formatting.
+ * `strb_push(&strb, char);`
+ * `strb_append(&strb, cstr);`
  *
- * -------------------------------------------------------------------------
- * IMPORTANT NOTICE:
- * This dynamic array implementation was originally sourced or heavily inspired
- * by code found here: `https://github.com/tsoding/nob.h`.
+ * `str strb_build(strb sb);`
  *
- * Please ensure you comply with the original license terms of that repository
- * if you are reusing or distributing this code.
- * -------------------------------------------------------------------------
+ * `strb_free(strb);`
+ *
+ * The following definitions can be used on a string view:
+ *
+ * `str str_cstr(char *cstr);`
+ *
+ * `STR_ARG(str)` used togheter with `STR_FMT` for `printf` formatting.
+ *
+ * `str str_substr(str s, size_t start, size_t end);`
+ * `bool str_starts_with(str a, str b);`
+ *
+ * It is recommended to construct a string using a string builder.
+ * Then continue using it as an immutable data structure by creating a view from it.
+ * And finally freeing the builder when no view exists anymore.
+ * Never create a string view, just get it returned by a function.
  */
 
 #ifndef STR_H_
 #define STR_H_
 
 #include "da.h"
+#include <stdbool.h>
 
 typedef struct {
     char *items;
     size_t count;
     size_t capacity;
-} str;
+} strb;
 
-#define str_push(str, char) da_append((str), (char))
+#define strb_push(strb, char) da_append((strb), (char))
 
-#define str_append(str, cstr) da_append_many((str), (cstr), strlen((cstr)))
+#define strb_append(strb, cstr) da_append_many((strb), (cstr), strlen((cstr)))
 
-#define str_free(str) da_free((str))
+#define strb_free(strb) da_free((strb))
 
 typedef struct {
     char *items;
     size_t count;
-} strv;
+} str;
 
-#define STRV_FMT "%.*s"
-#define STRV_ARG(strv) (int) (strv).count, (strv).items
+str strb_build(strb sb);
 
+str str_cstr(char *cstr);
+
+#define STR_FMT "%.*s"
+#define STR_ARG(str) (int) (str).count, (str).items
+
+str str_substr(str s, size_t start, size_t end);
+
+bool str_starts_with(str a, str b);
+
+#ifdef STR_IMPLEMENTATION
+
+str strb_build(strb sb) {
+    return (str) {
+        .items = sb.items,
+        .count = sb.count,
+    };
+}
+
+str str_cstr(char *cstr) {
+    return (str) {
+        .items = cstr,
+        .count = strlen(cstr),
+    };
+}
+
+str str_substr(str s, size_t start, size_t end) {
+    assert(start < end && end <= s.count);
+    return (str) {
+        .items = s.items + start,
+        .count = end - start,
+    };
+}
+
+bool str_starts_with(str a, str b) {
+    if(a.count < b.count) return false;
+    return memcmp(a.items, b.items, b.count) == 0;
+}
+
+#endif // STR_IMPLEMENTATION
 
 #endif // STR_H_
+
+
