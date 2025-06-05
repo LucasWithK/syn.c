25f3175f8ff5c82f0e5aa8dc65f2f262d2f86b31
LucasWithK <176577210+LucasWithK@users.noreply.github.com>
2025-06-05 14:08:22 +0200

    added dynamic array impl

---
+++ src/da.h
@@ -0,0 +1,75 @@
+/**
+ * This file provides the implementasions for a dynamic array data structure
+ * It requires a struct of the following shape for an array containing type `T`:
+ *
+ * typedef struct {
+ *  T *items;
+ *  size_t count;
+ *  size_t capacity;
+ * } ts;
+ *
+ * Then the following macros can be used:
+ *
+ * da_reserve(da, new_capacity);
+ * da_append(da, item);
+ * da_free(da);
+ * da_append_many(da, new_items, new_items_count);
+ * da_resize(da, size);
+ *
+ * -------------------------------------------------------------------------
+ * IMPORTANT NOTICE:
+ * This dynamic array implementation was originally sourced or heavily inspired
+ * by code found here: `https://github.com/tsoding/nob.h`.
+ *
+ * Please ensure you comply with the original license terms of that repository
+ * if you are reusing or distributing this code.
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef DA_H_
+#define DA_H_
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#ifndef DA_INIT_CAPACITY
+#define DA_INIT_CAPACITY 64
+#endif
+
+#define da_reserve(da, new_capacity)                                                    \
+    do {                                                                                \
+        if ((new_capacity) > (da)->capacity) {                                          \
+            if ((da)->capacity == 0) {                                                  \
+                (da)->capacity = DA_INIT_CAPACITY;                                      \
+            }                                                                           \
+            while ((new_capacity) > (da)->capacity) {                                   \
+                (da)->capacity *= 2;                                                    \
+            }                                                                           \
+            (da)->items = realloc((da)->items, (da)->capacity * sizeof(*(da)->items));  \
+            assert((da)->items != NULL);                                                \
+        }                                                                               \
+    } while (0)
+
+#define da_append(da, item)                     \
+    do {                                        \
+        da_reserve((da), (da)->count + 1);      \
+        (da)->items[(da)->count++] = (item);    \
+    } while (0)
+
+#define da_free(da) free((da).items)
+
+#define da_append_many(da, new_items, new_items_count)                                          \
+    do {                                                                                        \
+        da_reserve((da), (da)->count + (new_items_count));                                      \
+        memcpy((da)->items + (da)->count, (new_items), (new_items_count)*sizeof(*(da)->items)); \
+        (da)->count += (new_items_count);                                                       \
+    } while (0)
+
+#define da_resize(da, size)     \
+    do {                        \
+        da_reserve((da), size); \
+        (da)->count = (size);   \
+    } while (0)
+
+#endif // DA_H_
--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+#include "da.h"
+
+typedef struct {
+    int *items;
+    size_t count;
+    size_t capacity;
+} ints;
+
+
 int main(void) {
-    printf("Hello World!\n");
+    ints is = {0};
+
+    da_append(&is, 0);
+
+    int as[] = {1, 2};
+    da_append_many(&is, as, 2);
+
+    da_resize(&is, 15);
+
+    for(size_t i=0; i<is.count; ++i) {
+        printf("%d\n", is.items[i]);
+    }
+
+    da_free(is);
+
     return 0;
 }
